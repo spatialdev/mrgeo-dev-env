1635753563318921aef20b6f20e5a896f7d27b1e
Rich Gwozdz <rgwozdz@spatialdev.com>
2017-01-27 12:42:01 -0800

    Standardize MrGeo install.

--- shared/devops/ansible-install.sh
+++ shared/devops/ansible-install.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get update
 sudo apt-get install python-pip -y > /dev/null
-sudo pip install --upgrade-pip
+sudo pip install -U pip
 sudo pip install ansible==2.2.0.0
 ansible-playbook /home/vagrant/shared/devops/playbook.yml
